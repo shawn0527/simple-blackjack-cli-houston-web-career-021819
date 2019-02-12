d11bcc2ae9e8e170fe9f71717203d36aa74955d1
Shawn Ren <doggy-ren@hotmail.com>
2019-02-12 19:53:30 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -66,7 +66,7 @@ end
 def runner
   welcome
   card_total = initial_round
-  until card_total > 21 do
+  until card_total >= 21 do
     card_total = hit?(card_total)
     display_card_total(card_total)
   end
